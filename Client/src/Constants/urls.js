54f755cbc92d5a875965cf075deb519f84fd0fcb
LironSif <lironsifado@gmail.com>
2024-03-10 22:15:06 +0200

    test1

--- Client/src/Constants/urls.js
+++ Client/src/Constants/urls.js
@@ -3,7 +3,7 @@ const baseUrl = import.meta.env.VITE_SERVER_BASE_URL;
 
 const environment = import.meta.env.VITE_ENVIRONMENT;
 
-const renderUrl = `${import.meta.env.VITE_RENDER_URL}`;
+const renderUrl = import.meta.env.VITE_RENDER_URL;
 
 export const serverApiUrl =
   environment === "production" ? renderUrl : `${baseUrl}:${port}`;
