3ca5394a4e30ea5a9976f0e2898e9a5c7d281268
LironSif <lironsifado@gmail.com>
2024-03-10 16:38:15 +0200

    test

--- Client/src/components/AuthButtons/AuthOptions.jsx
+++ Client/src/components/AuthButtons/AuthOptions.jsx
@@ -4,7 +4,7 @@ import FacebookIcon from '../../assets/Login/Facebook_icon.svg';
 import './AuthOptions.css';
 
 const socialOptions = [
-  { name: 'Google', logo: GoogleLogo, authUrl: `${import.meta.env.VITE_SERVER_DEPLOY_URL}/auth/google` },
+  { name: 'Google', logo: GoogleLogo, authUrl: "/auth/google" },
   // { name: 'Google', logo: GoogleLogo, authUrl: `${import.meta.env.VITE_SERVER_URL}/auth/google` },
   { name: 'Facebook', logo: FacebookIcon, authUrl: `${import.meta.env.VITE_SERVER_DEPLOY_URL}/auth/facebook` },
   // { name: 'Facebook', logo: FacebookIcon, authUrl: `${import.meta.env.VITE_SERVER_URL}/auth/facebook` },
--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -11,10 +11,12 @@ router.get('/login/success', checkAuthStatus);
 
 // Google authentication and callback routes
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-router.get('/google/callback', passport.authenticate('google', { successRedirect: `${process.env.CLIENT_URL}`, failureRedirect: `${process.env.CLIENT_URL}/login` }));
+// router.get('/google/callback', passport.authenticate('google', { successRedirect: `${process.env.CLIENT_URL}`, failureRedirect: `${process.env.CLIENT_URL}/login` }));
+router.get('/google/callback', passport.authenticate('google', { successRedirect: `${process.env.CLIENT_DEPLOY_URL}`, failureRedirect: `${process.env.CLIENT_DEPLOY_URL}/login` }));
 
 // Facebook authentication and callback routes
 router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }));
-router.get('/facebook/callback', passport.authenticate('facebook',{ successRedirect: `${process.env.CLIENT_URL}`, failureRedirect: `${process.env.CLIENT_URL}/login` }));
+// router.get('/facebook/callback', passport.authenticate('facebook',{ successRedirect: `${process.env.CLIENT_URL}`, failureRedirect: `${process.env.CLIENT_URL}/login` }));
+router.get('/facebook/callback', passport.authenticate('facebook',{ successRedirect: `${process.env.CLIENT_DEPLOY_URL}`, failureRedirect: `${process.env.CLIENT_DEPLOY_URL}/login` }));
 
 export default router;
