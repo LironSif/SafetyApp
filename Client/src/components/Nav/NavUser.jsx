ab083446abae46ff7d211883516a2496df36d3e7
LironSif <lironsifado@gmail.com>
2024-03-20 20:57:46 +0200

    testing

--- Client/src/components/Nav/NavUser.jsx
+++ Client/src/components/Nav/NavUser.jsx
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAuth } from "../../context/AuthContext.jsx";
 import { useNavigate } from "react-router-dom";
 import logo from "../../assets/logo/techh.jpeg";
-import UserMenu from '../UserManu/UserMenu.jsx';
+import UserMenu from '../UserManu/UserMenu.jsx'; // Ensure the path is correct
 import './NavUser.css';
 
 const NavUser = () => {
   const { user } = useAuth();
   const navigate = useNavigate();
 
-  
-  localStorage.setItem('factoryId', user.factory);
+  useEffect(() => {
+    // Set factoryId to user.factory or an empty string if it's not set
+    localStorage.setItem('factoryId', user?.factory || '');
+  }, [user]); // Dependency array includes user, so this runs when user updates
+
   const avatarSrc = user?.avatar || logo;
   let userName = user?.email || 'Welcome, Guest!';
 
-  // Truncate userName to 9 characters + ellipsis if longer
   if (userName.length > 15) {
     userName = `${userName.substring(0, 15)}...`;
   }
 
   return (
     <div className='Navuser'>
-      <div className='user-profile' >
-        <img src={avatarSrc} onClick={()=> navigate("/")} alt="User Avatar" className='avatar'/>
-        <div  onClick={()=> navigate("/")} className='username'>{userName}</div>
+      <div className='user-profile'>
+        <img src={avatarSrc} onClick={() => navigate("/")} alt="User Avatar" className='avatar'/>
+        <div onClick={() => navigate("/")} className='username'>{userName}</div>
         <UserMenu />
       </div>
     </div>
