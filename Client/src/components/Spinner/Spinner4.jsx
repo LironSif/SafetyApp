186b37494341ed6ab28c1b3f78b99bf301828fb8
LironSif <130238629+LironSif@users.noreply.github.com>
2025-06-05 23:46:06 +0300

    Clean up spinner components and fix imports

--- Client/src/components/Spinner/Spinner4.jsx
+++ Client/src/components/Spinner/Spinner4.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
-import './Spinner4.css'; // Ensure this CSS file includes the new styles
-
-// Paths to the images might need to be updated based on your project structure
+import './Spinner4.css';
 
 import gogglesImg from '../../assets/spinner/goggles.png';
 import hammerImg from '../../assets/spinner/hammar.png';
 import hutImg from '../../assets/spinner/hut.png';
 
-const Spinner3 = () => {
+const Spinner4 = () => {
   return (
-    <div className="spinner3-container">
-      <div className="spinner3-triangle">
-        <img src={hutImg} className="spinner3-item" alt="Hut"/>
-        <img src={gogglesImg} className="spinner3-item spinner3-goggles" alt="Goggles"/>
-        <img src={hammerImg} className="spinner3-item spinner3-hammer" alt="Hammer"/>
+    <div className="spinner4-container">
+      <div className="spinner4-triangle">
+        <img src={hutImg} className="spinner4-item" alt="Hut" />
+        <img
+          src={gogglesImg}
+          className="spinner4-item spinner4-goggles"
+          alt="Goggles"
+        />
+        <img
+          src={hammerImg}
+          className="spinner4-item spinner4-hammer"
+          alt="Hammer"
+        />
       </div>
     </div>
   );
 };
 
-export default Spinner3;
+export default Spinner4;
